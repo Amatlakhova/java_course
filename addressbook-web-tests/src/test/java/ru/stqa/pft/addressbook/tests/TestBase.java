9f955da46de8f321e27d9c5e3a2c6708027b49e0
Amatlakhova <alina.matlakhova@gmail.com>
2019-05-06 21:24:54 -0400

    Moving description of test environment in configuration file

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/TestBase.java
@@ -8,7 +8,8 @@ import ru.stqa.pft.addressbook.appmanager.ApplicationManager;
 
 public class TestBase {
 
-  protected static final ApplicationManager app = new ApplicationManager(BrowserType.CHROME);
+  protected static final ApplicationManager app
+          = new ApplicationManager(System.getProperty("browser", BrowserType.CHROME));
   public WebDriver wd;
 
   @BeforeSuite(alwaysRun = true)
