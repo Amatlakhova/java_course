5f10c909a86af1da05e20accf819acf5c407da85
Amatlakhova <alina.matlakhova@gmail.com>
2019-05-16 23:16:07 +0300

    REST tests

---
+++ rests-sample/src/test/java/ru/stqa/pft/rest/Issue.java
@@ -0,0 +1,66 @@
+package ru.stqa.pft.rest;
+
+import java.util.Objects;
+
+public class Issue {
+
+  private int id;
+  private String subject;
+  private String description;
+  private int state;
+
+  public int getId() {
+    return id;
+  }
+
+  public Issue withId(int id) {
+    this.id = id;
+    return this;
+  }
+
+  public String getSubject() {
+    return subject;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    Issue issue = (Issue) o;
+    return id == issue.id &&
+            Objects.equals(subject, issue.subject) &&
+            Objects.equals(description, issue.description);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, subject, description);
+  }
+
+  public Issue withSubject(String subject) {
+    this.subject = subject;
+    return this;
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public Issue withDescription(String description) {
+    this.description = description;
+    return this;
+  }
+
+  @Override
+  public String toString() {
+    return "Issue{" +
+            "id=" + id +
+            ", subject='" + subject + '\'' +
+            ", description='" + description + '\'' +
+            '}';
+  }
+
+  public int getState() {
+    return state;
+  }
+}
---
+++ rests-sample/src/test/java/ru/stqa/pft/rest/RestTests.java
@@ -0,0 +1,71 @@
+package ru.stqa.pft.rest;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.reflect.TypeToken;
+import org.apache.http.client.fluent.Executor;
+import org.apache.http.client.fluent.Request;
+import org.apache.http.message.BasicNameValuePair;
+import org.testng.SkipException;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.Set;
+
+import static org.testng.Assert.assertEquals;
+
+public class RestTests {
+
+  @Test
+  public void testCreateIssue() throws IOException {
+    skipIfNotFixed(1280);
+    Set<Issue> oldIssues = getIssues();
+    Issue newIssue = new Issue()
+            .withSubject("Test issue")
+            .withDescription("New test issue");
+    int issueId = createIssue(newIssue);
+    Set<Issue> newIssues = getIssues();
+    oldIssues.add(newIssue.withId(issueId));
+    assertEquals(newIssues, oldIssues);
+  }
+
+  private Set<Issue> getIssues() throws IOException {
+    String json = getExecutor()
+            .execute(Request.Get("http://bugify.stqa.ru/api/issues.json"))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    JsonElement issues = parsed.getAsJsonObject().get("issues");
+    return new Gson().fromJson(issues, new TypeToken<Set<Issue>>() {}.getType());
+  }
+
+  private Executor getExecutor() {
+    return Executor.newInstance().auth("288f44776e7bec4bf44fdfeb1e646490", "");
+  }
+
+  private int createIssue(Issue newIssue) throws IOException {
+    String json = getExecutor()
+            .execute(Request.Post("http://bugify.stqa.ru/api/issues.json")
+                    .bodyForm(new BasicNameValuePair("subject", newIssue.getSubject()),
+                              new BasicNameValuePair("description", newIssue.getDescription())))
+            .returnContent().asString();
+    JsonElement parsed = new JsonParser().parse(json);
+    return parsed.getAsJsonObject().get("issue_id").getAsInt();
+  }
+
+  public void skipIfNotFixed(int issueId) throws IOException {
+    if (isIssueOpen(issueId)) {
+      throw new SkipException("Ignored because of issue " + issueId);
+    }
+  }
+
+  private boolean isIssueOpen(int issueId) throws IOException {
+    Set<Issue> issues = getIssues();
+    for (Issue issue : issues) {
+      if (issue.getId() == issueId) {
+        return issue.getState() == 0 || issue.getState() == 1;
+      }
+    }
+    return false;
+  }
+}
